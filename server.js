5233dcdf7addd40ae1340295c0dcccc6e12ff255
Michael Tinning <m.t.tinning@gmail.com>
2016-03-11 12:37:33 +0000

    Adding index.html

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const dir = process.argv[2];
+const port = 8080;
+const app = express();
+
+app.use(express.static(dir));
+app.listen(port);
+
+console.log('\n Serving directory /' + dir + ' on port ' + port);
