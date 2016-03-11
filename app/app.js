0be352c21e1cc5a955a74b74fe220824287a9a58
Michael Tinning <m.t.tinning@gmail.com>
2016-03-11 12:59:52 +0000

    Adding TechPoll component and routing

--- app/app.js
+++ app/app.js
@@ -1,7 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import {Router, Route, hashHistory} from 'react-router'
+import {App} from './components/App'
+import {TechPoll} from './components/TechPoll'
+
+const routes = (
+<Route component={App}>
+  <Route path="/" component={TechPoll}/>
+</Route>
+)
 
 ReactDOM.render(
-  <div>Hello, world!</div>,
+  <Router history={hashHistory}>{routes}</Router>,
   document.getElementById('app')
 )
---
+++ app/components/TechPoll.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export class TechPoll extends React.Component {
+  render() {
+    return <h2>Welcome to Tech Poll!</h2>
+  }
+}
