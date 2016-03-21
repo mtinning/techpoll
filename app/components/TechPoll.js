efb388ed667534f532eba21bb5dbf7a7ff143de2
Michael Tinning <m.t.tinning@gmail.com>
2016-03-21 10:18:08 +0000

    Working add comment

--- app/components/TechPoll.js
+++ app/components/TechPoll.js
@@ -1,12 +1,25 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
 import { TechCategoryListContainer } from './TechCategoryList'
 import { AddNewTechContainer } from './AddNewTech'
+import { AddVoteContainer } from './AddVote'
 
-export const TechPoll = () => (
+export const TechPoll = ({ addVoteOpen }) => (
   <div>
     <h2>{'TechPoll'}</h2>
     <TechCategoryListContainer />
     <AddNewTechContainer />
+    { addVoteOpen ? <AddVoteContainer /> : null }
   </div>
 )
+
+function mapStateToProps(state) {
+  return {
+    addVoteOpen: state.has('currentVote')
+  }
+}
+
+export const TechPollContainer = connect(
+  mapStateToProps
+)(TechPoll)
